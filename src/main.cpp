b67d8c24cef1699adac8f4abc75727966463e26b
ginggonzalez <ginggonzalez3@gmail.com>
2021-03-03 10:38:51 -0800

    Refactor Bluetooth driver. Transmit game details whenever a score is made.

--- src/main.cpp
+++ src/main.cpp
@@ -127,24 +127,39 @@ void bluetooth_transmit_state_task(void *parameters);
 /**************************************
  * BLE Variables
  **************************************/
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID        "4a220a7a-8094-435b-8b3e-b19682b41381"
+#define CHARACTERISTIC_UUID_RX "638b6661-6196-4efc-82f4-e90a59e6e8a3"
+#define CHARACTERISTIC_UUID_TX  "30ab87d1-2e52-4874-a519-888c6fc54bcc"
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
+uint8_t is_ble_client_connected = 0;
+BLECharacteristic *tx_characteristic;
+uint32_t tx_value;
 
+/**************************************
+ * BLE Classes
+ **************************************/
 class ServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("Device Connected");
+  void onConnect(BLEServer *ble_server) {
+    // Only allow two simultaneous connections
+    if (++is_ble_client_connected < 2) {
       BLEDevice::startAdvertising();
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
     }
+  }
+
+  void onDisconnect(BLEServer *ble_server) {
+    --is_ble_client_connected;
+  }
+};
+
+class CharacteristicCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *characteristic) {
+    std::string rx_value = characteristic->getValue();
+    const char* rx_decoded = rx_value.c_str();
+
+    if (rx_value.length() > 0) {
+      Serial.printf("%c, %c, %c, %c\n", rx_decoded[0], rx_decoded[1], rx_decoded[2], rx_decoded[3]);
+    }
+  }
 };
 
 /**************************************
@@ -166,7 +181,7 @@ void setup() {
 
   attachInterrupt(BUTTON_PIN, isr, FALLING);
 
-  data_dump_enabled = 1;
+  data_dump_enabled = 0;
   sets_count = 5;
 
   identify_hit_smphr = xSemaphoreCreateBinary();
@@ -183,7 +198,7 @@ void setup() {
 
   // Core 1 Tasks
   xTaskCreatePinnedToCore(binary_data_dump_task, "binary_data_dump_task", 4096, NULL, 1, NULL, 1);
-  // xTaskCreatePinnedToCore(ble_task, "ble_task", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(ble_task, "ble_task", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
@@ -329,6 +344,8 @@ void table_tennis_dynamics_task(void *parameters) {
         else {
           server_changed = handle_serve_switch(&server_score, &receiver_score, &server, 2);
         }
+
+        xSemaphoreGive(transmit_state_smphr);
 
         print_game_details(black_score, red_score, server, server_changed, score_status);
       }
@@ -623,61 +640,36 @@ void IRAM_ATTR isr() {
  * BLE Tasks
  **************************************/
 void ble_task(void* parameters) {
-  // Create the BLE Device
-  BLEDevice::init("ESP32");
+  BLEDevice::init("UmpireAI");
+  BLEServer *ble_server = BLEDevice::createServer();
 
-  // Create the BLE Server
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ServerCallbacks());
+  ble_server->setCallbacks(new ServerCallbacks());
+  BLEService *service = ble_server->createService(SERVICE_UUID);
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(SERVICE_UUID);
+  // Create service for data transmit
+  tx_characteristic = service->createCharacteristic(
+    CHARACTERISTIC_UUID_TX,
+    BLECharacteristic::PROPERTY_NOTIFY
+    );
+ 
+  tx_characteristic->addDescriptor(new BLE2902());
 
-  // Create a BLE Characteristic
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
+  // Create service for data receive
+  BLECharacteristic *rx_characteristic = service->createCharacteristic(
+    CHARACTERISTIC_UUID_RX,
+    BLECharacteristic::PROPERTY_WRITE
+    );
+  rx_characteristic->setCallbacks(new CharacteristicCallbacks());
 
-  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
-  // Create a BLE Descriptor
-  pCharacteristic->addDescriptor(new BLE2902());
+  service->start();
+  ble_server->getAdvertising()->start();
 
-  // Start the service
-  pService->start();
-
-  // Start advertising
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
-  BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
-
-  uint32_t data = 0;
   while (1) {
-    data = 0;
-    data |= (black_score) | (red_score << 8) | (server << 16) | (score_status << 24);
-    // notify changed value
-    if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&data, 4);
-        pCharacteristic->notify();
-        delay(1000); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
-    }
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
+    if (xSemaphoreTake(transmit_state_smphr, portMAX_DELAY) && is_ble_client_connected) {
+      tx_value = ((uint32_t)score_status) | ((uint32_t)black_score << 8) | ((uint32_t)red_score << 16) | ((uint32_t)server << 24);
+      Serial.printf("Transmitting %d\n", tx_value);
+      tx_characteristic->setValue(tx_value);
+      tx_characteristic->notify();
     }
   }
 }
