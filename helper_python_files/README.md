953a18c08c4de8e3590871cc58b07eb28ef83501
ginggonzalez <ginggonzalez3@gmail.com>
2021-03-30 11:18:32 -0700

    feat: Add python helper files for analyzing data dump

---
+++ helper_python_files/README.md
@@ -0,0 +1,5 @@
+## Usage
+
+python read_bin.py <name_of_binfile.bin>
+
+Note: You may need to do pip install first to install the required packages.
--- helper_python_files/read_bin.py
+++ helper_python_files/read_bin.py
@@ -1,10 +1,11 @@
 import os
+import sys
 import struct
 import matplotlib.pyplot as plt
 import numpy as np
 
-file = open('data_dump.bin', 'rb')
-file_size = os.path.getsize('data_dump.bin')
+file = open(sys.argv[1], 'rb')
+file_size = os.path.getsize(sys.argv[1])
 data_points_total_count = file_size / 4
 data_label_points = 1
 data_points_per_vibration = 100 + data_label_points
@@ -12,12 +13,11 @@ data_points_per_vibration = 100 + data_label_points
 numbers = list(struct.unpack('f' * data_points_total_count, file.read(4 * data_points_total_count)))
 file.close()
 
-with open('readable.txt', 'w') as f:
+with open(os.path.splitext(sys.argv[1])[0] + '.txt', 'w') as f:
 	for index, item in enumerate(numbers):
 		f.write("%s: %s, " % (index + 1, item))
 		if (index != 0 and (index + 1) % data_points_per_vibration == 0):
 			f.write("\n")
-
 
 plt.plot(numbers)
 plt.show()
